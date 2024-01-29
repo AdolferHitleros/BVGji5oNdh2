9b4eaa302c85ac290874080396fe280cc2b49aa7
AdolferHitleros <157637317+AdolferHitleros@users.noreply.github.com>
2024-01-29 19:03:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -7,9 +7,7 @@ def het_ip_user():
     with open('info_users.txt', 'a+', encoding='utf-8') as f:
         if request.remote_addr not in f:
             f.write(f'{request.remote_addr} \n')
-    return jsonify({'Your ip': request.remote_addr}), 200
-
-
+    return render_template('index.html') 
 
 
 if __name__ == '__main__':
